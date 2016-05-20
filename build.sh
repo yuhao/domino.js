0e2e74091958c451f247990806e502710fa4d1ab
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-05-20 10:29:17 -0500

    Allow specifying inspector name when build.

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
-node_modules/vulcanize/bin/vulcanize -o din.html core/main.html
+inspector=$1
 
+if [ -z "$inspector" ]; then
+  inspector="inspector"
+  echo "Use the default inspector"
+else
+  echo "Use inspector/$inspector.html as the inspector"
+fi
+
+cp inspector/$inspector.html core/inspector.html
+
+echo "Generating content script"
+node_modules/vulcanize/bin/vulcanize -o din.html core/main.html
 node extractJS.js | awk '
 BEGIN {
   print "injectScript(function() {";
