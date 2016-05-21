8d252618bc307103bf113dcdee658ac74ab24215
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2016-05-20 20:50:33 -0500

    Directly specify the inspector file name in build.sh.

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 inspector=$1
 
 if [ -z "$inspector" ]; then
-  inspector="inspector"
+  inspector="inspector/inspector.html"
   echo "Use the default inspector"
 else
-  echo "Use inspector/$inspector.html as the inspector"
+  echo "Use $inspector as the inspector"
 fi
 
-cp inspector/$inspector.html core/inspector.html
+cp $inspector core/inspector.html
 
 echo "Generating content script"
 node_modules/vulcanize/bin/vulcanize -o din.html core/main.html
